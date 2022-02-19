4a3c5a6dc92b04dd4228206c8ff7377aeaaa74a2
Dianliang233 <dianliang233@gmail.com>
2022-02-19 22:47:36 +0800

    Usable now

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,22 @@
 import type {LanguageId} from './register';
 import type {ScopeName, TextMateGrammar, ScopeNameInfo} from './providers';
-
-// Recall we are using MonacoWebpackPlugin. According to the
-// monaco-editor-webpack-plugin docs, we must use:
-//
-// import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
-//
-// instead of
-//
-// import * as monaco from 'monaco-editor';
-//
-// because we are shipping only a subset of the languages.
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import {createOnigScanner, createOnigString, loadWASM} from 'vscode-oniguruma';
 import {SimpleLanguageInfoProvider} from './providers';
 import {registerLanguages} from './register';
 import {rehydrateRegexps} from './configuration';
 import VsCodeDarkTheme from './vs-dark-plus-theme';
+import * as Y from 'yjs';
+import * as awarenessProtocol from 'y-protocols/awareness.js';
+import {WebrtcProvider} from 'y-webrtc';
+// @ts-expect-error
+import {MonacoBinding} from 'y-monaco';
 
 interface DemoScopeNameInfo extends ScopeNameInfo {
   path: string;
 }
 
-main('python');
+main('bbcode');
 
 async function main(language: LanguageId) {
   // In this demo, the following values are hardcoded to support Python using
@@ -42,33 +36,17 @@ async function main(language: LanguageId) {
   // change the call to main() above to pass your LanguageId.
   const languages: monaco.languages.ILanguageExtensionPoint[] = [
     {
-      id: 'python',
-      extensions: [
-        '.py',
-        '.rpy',
-        '.pyw',
-        '.cpy',
-        '.gyp',
-        '.gypi',
-        '.pyi',
-        '.ipy',
-        '.bzl',
-        '.cconf',
-        '.cinc',
-        '.mcconf',
-        '.sky',
-        '.td',
-        '.tw',
-      ],
-      aliases: ['Python', 'py'],
-      filenames: ['Snakefile', 'BUILD', 'BUCK', 'TARGETS'],
-      firstLine: '^#!\\s*/?.*\\bpython[0-9.-]*\\b',
+      id: 'bbcode',
+      extensions: ['.bbcode'],
+      aliases: ['BBC'],
+      filenames: [],
+      firstLine: '',
     },
   ];
   const grammars: {[scopeName: string]: DemoScopeNameInfo} = {
-    'source.python': {
-      language: 'python',
-      path: 'MagicPython.tmLanguage.json',
+    'text.bbcode': {
+      language: 'bbcode',
+      path: 'bbcode.tmLanguage.json',
     },
   };
 
@@ -119,7 +97,240 @@ async function main(language: LanguageId) {
     throw Error(`could not find element #${id}`);
   }
 
-  monaco.editor.create(element, {
+  monaco.languages.registerCompletionItemProvider('bbcode', {
+    provideCompletionItems: (model, position) => {
+      var word = model.getWordUntilPosition(position);
+      var range = {
+        startLineNumber: position.lineNumber,
+        endLineNumber: position.lineNumber,
+        startColumn: word.startColumn,
+        endColumn: word.endColumn,
+      };
+      return {
+        suggestions: [
+          {
+            label: '.start',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '文章的起始部分',
+            insertText: `[postbg]bg3.png[/postbg][align=center][img=965,412]\${1:https://www.minecraft.net/content/dam/games/minecraft/screenshots/candle-header.jpg}[/img]
+
+[backcolor=Black][color=White][b]\${2:CATEGORY}[/b][/color][/backcolor]
+
+[size=6][b][color=Silver]\${3:Title}[/color][/b][/size]
+[size=6][b]\${4:标题}[/b][/size]
+
+[size=4][b][color=Silver]\${5:Subtitle}[/color][/b][/size]
+[size=4][b]\${6:副标题}[/b][/size][/align]`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+          {
+            label: '.tabs',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '添加两个 `[intent]`',
+            insertText: `[indent][indent]\n$1\n[/indent][/indent]`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+          {
+            label: '.p',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '新段落',
+            insertText: `[size=2][color=Silver]$1[/size]\n$2`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+          {
+            label: '.url',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '新链接（译文）',
+            insertText: `[url=$1][color=#388d40]$2[/color][/url]`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+          {
+            label: '.url.orig',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '新链接（原文）',
+            insertText: `[url=$1][color=Silver]$2[/color][/url]`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+          {
+            label: '.code',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '代码格式（译文）',
+            insertText: `[backcolor=#f1edec][color=译][font=SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace]$1[/font][/color][/backcolor]`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+          {
+            label: '.code.orig',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '代码格式（原文）',
+            insertText: `[backcolor=#f1edec][color=Silver][font=SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace]$1[/font][/color][/backcolor]`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+          {
+            label: '.h2',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '创建新二级段落',
+            insertText: `[size=5][b][color=Silver]$1[/color][/b][/size]
+[size=5][b]$2[/b][/size]`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+          {
+            label: '.h3',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '创建新三级段落',
+            insertText: `[size=4][b][color=Silver]$1[/color][/b][/size]
+[size=5][b]$2[/b][/size]`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+          {
+            label: '.h4',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '创建新四级段落',
+            insertText: `[size=3][b][color=Silver]$1[/color][/b][/size]
+[size=5][b]$2[/b][/size]`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+          {
+            label: '.h5',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '创建新五级段落',
+            insertText: `[size=2][b][color=Silver]$1[/color][/b][/size]
+[size=5][b]$2[/b][/size]`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+          {
+            label: '.li',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '创建新六级段落',
+            insertText: `[list]
+[*][color=Silver]$1[/color]
+[*]$2
+[/list]`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+          {
+            label: '.img',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '创建单个带有 caption 的图片',
+            insertText: `[align=center][img]$1[/img]
+[b][size=2][color=Silver]$2[/size]
+$3[/b][/align]`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+          {
+            label: '.quote',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '创建一个普通引用',
+            insertText: `[quote][float=left][img=53,92]$1[/img][/float][size=2][color=Silver]$2[/color][/size]
+$2
+
+ —— $3
+[/quote]`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+          {
+            label: '.quote',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '创建一个普通引用',
+            insertText: `[quote][float=left][img=53,92]$1[/img][/float][size=2][color=Silver]$2[/color][/size]
+$2
+
+ —— $3
+[/quote]`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+          {
+            label: '.greenquote',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '创建一个绿色引用',
+            insertText: `[indent][size=4][color=SeaGreen][b]|[/b][/color][color=Silver]$1[/color][/size][/indent]
+[indent][size=4][color=SeaGreen][b]|[/b][/color]$2[/size][/indent]`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+          {
+            label: '.video',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '嵌入视频',
+            insertText: `[align=center][media=x,500,375]$1[/media]
+视频提供：\${2:@广药} [/align]`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+          {
+            label: '.video.ytb',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '连接至 YouTube 视频',
+            insertText: `[align=center][url=$1][img]$2[/img][/url]
+（点击图片跳转到 Youtube 查看视频）[/align]`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+          {
+            label: '.ending',
+            additionalTextEdits: [{range, text: null}],
+            kind: monaco.languages.CompletionItemKind.Snippet,
+            documentation: '文章的结束部分',
+            insertText: `[float=left][img=82,121]\${1:https://www.minecraft.net/content/dam/archive/673fbce045729846ea80943491f2a4ab-PerAvatar.png}[/img][/float]
+
+[b]【$2 译自[url=$3][color=#388d40][u]\${3:官网} $4 年 $5 月 $6 日发布的 $7[/u][/color][/url]；原作者 $8】[/b]`,
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+          },
+        ],
+      };
+    },
+  });
+
+  const ydoc = new Y.Doc();
+  const yprovider = new WebrtcProvider('monaco', ydoc, {
+    signaling: [
+      'ws://bb.guangyaostore.com',
+      'wss://signaling.yjs.dev',
+      'wss://y-webrtc-signaling-eu.herokuapp.com',
+      'wss://y-webrtc-signaling-us.herokuapp.com',
+    ],
+    password: null,
+    awareness: new awarenessProtocol.Awareness(ydoc),
+    maxConns: 20 + Math.floor(Math.random() * 15),
+    filterBcConns: true,
+    peerOpts: {},
+  });
+  const type = ydoc.getText('monaco');
+
+  const editor = monaco.editor.create(element, {
     value,
     language,
     theme: 'vs-dark',
@@ -128,6 +339,13 @@ async function main(language: LanguageId) {
     },
   });
   provider.injectCSS();
+
+  const monacoBinding = new MonacoBinding(
+    type,
+    editor.getModel(),
+    new Set([editor]),
+    yprovider.awareness,
+  );
 }
 
 // Taken from https://github.com/microsoft/vscode/blob/829230a5a83768a3494ebbc61144e7cde9105c73/src/vs/workbench/services/textMate/browser/textMateService.ts#L33-L40
@@ -145,7 +363,7 @@ async function loadVSCodeOnigurumWASM(): Promise<Response | ArrayBuffer> {
 }
 
 function getSampleCodeForLanguage(language: LanguageId): string {
-  if (language === 'python') {
+  if (language === 'bbcode') {
     return `\
 import foo
 
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+import monacoEditorPlugin from 'vite-plugin-monaco-editor';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      reactivityTransform: true,
+    }),
+    monacoEditorPlugin,
+  ],
+  define: {'process.env': {}},
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
+  },
+});
